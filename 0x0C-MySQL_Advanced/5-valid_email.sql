2209d081ec51cfd90beb38ec7a8a2c26ba73080a
John Nyokabi <jwnyokabi@gmail.com>
2023-07-15 20:23:38 +0300

    Advanced MySQL

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+--Script that creates a trigger that resets an attribute
+CREATE trigger reset_valid
+before update ON users
+for each row
+BEGIN
+    if NEW.email <> OLD.email
+    THEN SET NEW.valid_email = 0
+    end if;
+END //
+delimiter ;
